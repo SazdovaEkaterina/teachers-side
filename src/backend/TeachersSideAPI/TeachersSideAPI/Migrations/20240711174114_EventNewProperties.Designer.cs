9bb05f94b9f0fa25f53ed1e77d1db783cadcfc1e
Jana Ristovska <ristovskajana@gmail.com>
2024-07-11 19:54:44 +0200

    Added new properties in Event model.
    Added image and location properties in Event.

--- src/backend/TeachersSideAPI/TeachersSideAPI/Migrations/20240711174114_EventNewProperties.Designer.cs
+++ src/backend/TeachersSideAPI/TeachersSideAPI/Migrations/20240711174114_EventNewProperties.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TeachersSideAPI.Persistence;
@@ -11,9 +12,10 @@ using TeachersSideAPI.Persistence;
 namespace TeachersSideAPI.Migrations
 {
     [DbContext(typeof(TeachersSideContext))]
-    partial class TeachersSideContextModelSnapshot : ModelSnapshot
+    [Migration("20240711174114_EventNewProperties")]
+    partial class EventNewProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
