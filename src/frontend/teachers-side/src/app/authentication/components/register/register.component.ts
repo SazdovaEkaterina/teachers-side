3c021b9f0ced6fb9d02c92a8567e3d67b417316d
AnastasijaSpasovska <102061591+AnastasijaSpasovska@users.noreply.github.com>
2024-09-11 17:26:36 +0200

    Feature: Register Page Design (#19)
    * initial commit
    
    * css validation changes
    
    ---------
    
    Co-authored-by: Ekaterina Sazdova <74919977+SazdovaEkaterina@users.noreply.github.com>

--- src/frontend/teachers-side/src/app/authentication/components/register/register.component.ts
+++ src/frontend/teachers-side/src/app/authentication/components/register/register.component.ts
@@ -9,53 +9,70 @@ import { IValidatorRule } from '../../models/validator-rule';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit, OnDestroy {
-
   public formGroup: FormGroup = this.formBuilder.group({});
-  public errorMessage: string = "";
+  public errorMessage: string = '';
 
   public passwordValidatorRules: IValidatorRule[] = [
     {
       id: 0,
       text: 'Password must be between 8 and 20 characters long.',
-      pattern: new RegExp('^[a-zA-Z0-9.*\\W.*]{8,20}$')
+      pattern: new RegExp('^[a-zA-Z0-9.*\\W.*]{8,20}$'),
     },
     {
       id: 1,
       text: 'Password must contain a lowercase letter.',
-      pattern: new RegExp('.*[a-z]')
+      pattern: new RegExp('.*[a-z]'),
     },
     {
       id: 2,
       text: 'Password must contain an uppercase letter.',
-      pattern: new RegExp('.*[A-Z]')
+      pattern: new RegExp('.*[A-Z]'),
     },
     {
       id: 3,
       text: 'Password must contain a number.',
-      pattern: new RegExp('.*\\d')
+      pattern: new RegExp('.*\\d'),
     },
     {
       id: 4,
       text: 'Password must contain a special character.',
-      pattern: new RegExp('.*\\W.*')
+      pattern: new RegExp('.*\\W.*'),
     },
-  ]
+  ];
 
-  private userNameRegex: string = "^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$";
-  private emailRegex: string = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
-  private passwordRegex: string = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,20}$";
+  public userNameValidatorRules: IValidatorRule[] = [
+    {
+      id: 0,
+      text: 'User name must be between 8 and 20 characters long.',
+      pattern: new RegExp('^[a-zA-Z0-9.*\\W.*]{8,20}$'),
+    },
+  ];
+
+  public emailValidatorRules: IValidatorRule[] = [
+    {
+      id: 0,
+      text: 'Email must be a valid email address.',
+      pattern: new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+    },
+  ];
+
+  private userNameRegex: string =
+    '^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$';
+  private emailRegex: string = '^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$';
+  private passwordRegex: string =
+    '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,20}$';
 
   private ngUnsubscribe = new Subject<void>();
 
   constructor(
     @Inject(FormBuilder) private readonly formBuilder: FormBuilder,
-    @Inject(AuthenticationService) private readonly authenticationService: AuthenticationService,
-    @Inject(Router) private router: Router,
-    ) {
-  }
+    @Inject(AuthenticationService)
+    private readonly authenticationService: AuthenticationService,
+    @Inject(Router) private router: Router
+  ) {}
 
   public ngOnInit(): void {
     this.buildForm();
@@ -63,25 +80,23 @@ export class RegisterComponent implements OnInit, OnDestroy {
 
   public submit() {
     const payload = this.formGroup.getRawValue() as RegisterModel;
-    this.authenticationService.register(payload)
+    this.authenticationService
+      .register(payload)
       .pipe(
         tap(() => {
           this.navigateToLogin();
         }),
         catchError((error) => {
-          if(error.status === 409){
+          if (error.status === 409) {
             this.errorMessage = error.error;
-          }
-          else if(error.status === 400){
-            this.errorMessage = error.errors.toString();
-          }
-          else {
-            this.errorMessage = "Something went wrong, please try again.";
+          } else {
+            this.errorMessage = 'Something went wrong, please try again.';
           }
           return EMPTY;
         }),
         takeUntil(this.ngUnsubscribe)
-      ).subscribe();
+      )
+      .subscribe();
   }
 
   public navigateToLogin() {
@@ -97,10 +112,15 @@ export class RegisterComponent implements OnInit, OnDestroy {
     this.formGroup = this.formBuilder.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
-      userName: ['', [Validators.required, Validators.pattern(this.userNameRegex)]],
+      userName: [
+        '',
+        [Validators.required, Validators.pattern(this.userNameRegex)],
+      ],
       email: ['', [Validators.required, Validators.pattern(this.emailRegex)]],
-      password: ['', [Validators.required, Validators.pattern(this.passwordRegex)]],
+      password: [
+        '',
+        [Validators.required, Validators.pattern(this.passwordRegex)],
+      ],
     });
   }
-
 }
