6f545310a8f73eff790f116131ab8c6f5a8bbc98
AnastasijaSpasovska <anastasija.spasovska@students.finki.ukim.mk>
2024-09-18 00:18:52 +0200

    initial commit

--- src/frontend/teachers-side/src/app/app-routing.module.ts
+++ src/frontend/teachers-side/src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { RegisterComponent } from './authentication/components/register/register
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './platform/components/home/home.component';
 import { AuthGuard } from './authentication/guards/auth.guard';
+import { EventsTabComponent } from './platform/components/events-tab/events-tab.component';
+
 
 const routes: Routes = [
   {
@@ -19,6 +21,14 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'eventsTab',
+    component: EventsTabComponent 
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
 
