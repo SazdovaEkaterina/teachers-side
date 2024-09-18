d5502568a3b7445ebadc7f1fdd41c1c97b9ca83a
Ekaterina Sazdova <74919977+SazdovaEkaterina@users.noreply.github.com>
2024-09-18 19:10:40 +0200

    feat: comment api crud

--- src/backend/TeachersSideAPI/TeachersSideAPI/Service/Implementation/CommentService.cs
+++ src/backend/TeachersSideAPI/TeachersSideAPI/Service/Implementation/CommentService.cs
@@ -1,13 +1,88 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using TeachersSideAPI.Domain.DTO;
+using TeachersSideAPI.Domain.Models;
 using TeachersSideAPI.Persistence.Repositories;
+using TeachersSideAPI.Service.Exceptions;
 
 namespace TeachersSideAPI.Service.Implementation;
 
 public class CommentService : ICommentService
 {
     private readonly ICommentRepository _commentRepository;
+    private readonly IPostRepository _postRepository;
+    private readonly IForumRepository _forumRepository;
 
-    public CommentService(ICommentRepository commentRepository)
+    private readonly IMapper _mapper;
+    private readonly UserManager<Teacher> _userManager;
+
+    public CommentService(
+        ICommentRepository commentRepository,
+        IMapper mapper,
+        UserManager<Teacher> userManager,
+        IForumRepository forumRepository,
+        IPostRepository postRepository
+    )
     {
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        _forumRepository = forumRepository ?? throw new ArgumentNullException(nameof(forumRepository));
+        _postRepository = postRepository ?? throw new ArgumentNullException(nameof(postRepository));
         _commentRepository = commentRepository ?? throw new ArgumentNullException(nameof(commentRepository));
+    }
+
+    public async Task<IEnumerable<CommentDto>> GetAllForPostAsync(int postId)
+    {
+        var comments = await _commentRepository.GetAllByPostIdAsync(postId);
+        return _mapper.Map<IEnumerable<CommentDto>>(comments);
+    }
+
+    public async Task<bool> SaveAsync(CommentDto commentDto)
+    {
+        var comment = _mapper.Map<Comment>(commentDto);
+        comment.Post.Forum = await _forumRepository.GetAsync(commentDto.Post.Forum.Id)
+                             ?? throw new ForumNotFoundException(
+                                 $"Forum with ID: {commentDto.Post.Forum.Id} not found.");
+        comment.Post = await _postRepository.GetAsync(commentDto.Post.Id)
+                       ?? throw new ForumNotFoundException($"Post with ID: {commentDto.Post.Id} not found.");
+        comment.Creator = await _userManager.FindByEmailAsync(commentDto.Creator.Email)
+                          ?? throw new UserNotFoundException($"User with email {commentDto.Creator.Email} not found");
+        comment.Title = comment.Title;
+        comment.Content = comment.Content;
+        comment.DateCreated = DateTime.UtcNow;
+        comment.LastEdited = DateTime.UtcNow;
+        return await _commentRepository.SaveAsync(comment);
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var comment = await _commentRepository.GetAsync(id);
+
+        if (comment == null)
+            return false;
+
+        return await _commentRepository.DeleteAsync(comment);
+    }
+
+    public async Task<bool> EditAsync(int id, CommentDto commentDto)
+    {
+        var comment = await _commentRepository.GetAsync(id);
+
+        if (comment == null)
+            return false;
+        
+        comment.Post.Forum = await _forumRepository.GetAsync(commentDto.Post.Forum.Id)
+                             ?? throw new ForumNotFoundException(
+                                 $"Forum with ID: {commentDto.Post.Forum.Id} not found.");
+        comment.Post = await _postRepository.GetAsync(commentDto.Post.Id)
+                       ?? throw new ForumNotFoundException($"Post with ID: {commentDto.Post.Id} not found.");
+        comment.Creator = await _userManager.FindByEmailAsync(commentDto.Creator.Email)
+                          ?? throw new UserNotFoundException($"User with email {commentDto.Creator.Email} not found");
+        comment.Title = comment.Title;
+        comment.Content = comment.Content;
+        comment.DateCreated = DateTime.UtcNow;
+        comment.LastEdited = DateTime.UtcNow;
+
+        return await _commentRepository.SaveChangesAsync();
     }
 }
--- src/backend/TeachersSideAPI/TeachersSideAPI/Web/Controllers/CommentsController.cs
+++ src/backend/TeachersSideAPI/TeachersSideAPI/Web/Controllers/CommentsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using TeachersSideAPI.Domain.DTO;
 using TeachersSideAPI.Service;
 
 namespace TeachersSideAPI.Web.Controllers;
@@ -12,5 +13,39 @@ public class CommentsController : ControllerBase
     public CommentsController(ICommentService commentService)
     {
         _commentService = commentService ?? throw new ArgumentNullException(nameof(commentService));
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CommentDto>>> GetAllAsync([FromQuery]int postId)
+    {
+        return Ok(await _commentService.GetAllForPostAsync(postId));
+    }
+
+    [HttpPost("add")]
+    public async Task<ActionResult<bool>> AddAsync([FromBody] CommentDto commentDto)
+    {
+        try
+        {
+            var result = await _commentService.SaveAsync(commentDto);
+            return Ok(result);
+        }
+        catch (Exception exception)
+        {
+            return Conflict();
+        }
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<bool>> DeleteAsync([FromRoute] int id)
+    {
+        var result = await _commentService.DeleteAsync(id);
+        return result ? Ok(result) : NotFound();
+    }
+    
+    [HttpPost("{id}/edit")]
+    public async Task<ActionResult<bool>> EditAsync([FromRoute]int id, [FromBody] CommentDto commentDto)
+    { 
+        var result = await _commentService.EditAsync(id, commentDto);
+        return result ? Ok(result) : NotFound();
     }
 }
