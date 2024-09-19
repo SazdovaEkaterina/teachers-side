c2c39a4bb6de5f2b29bae664550ed2a72a68ed80
AnastasijaSpasovska <anastasija.spasovska@students.finki.ukim.mk>
2024-09-19 17:32:40 +0200

    initial commit

--- src/frontend/teachers-side/src/app/platform/components/event-card/event-card.component.ts
+++ src/frontend/teachers-side/src/app/platform/components/event-card/event-card.component.ts
@@ -16,6 +16,18 @@ export class EventCardComponent {
     startDate: new Date(),
     endDate: new Date(),
   };
+  formatDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    };
+    return new Intl.DateTimeFormat('en-GB', options).format(date);
+  }
 
   public goToEventDetails () {
     
