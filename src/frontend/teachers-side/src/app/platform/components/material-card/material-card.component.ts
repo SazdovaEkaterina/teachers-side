2188da846b45e876f3a32b97977ed5cf68181f07
AnastasijaSpasovska <anastasija.spasovska@students.finki.ukim.mk>
2024-09-20 00:32:13 +0200

    initial commit

--- src/frontend/teachers-side/src/app/platform/components/material-card/material-card.component.ts
+++ src/frontend/teachers-side/src/app/platform/components/material-card/material-card.component.ts
@@ -30,6 +30,18 @@ export class MaterialCardComponent {
     name: '',
     category: 0,
   };
+  public formatDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    };
+    return new Intl.DateTimeFormat('en-GB', options).format(date);
+  }
   @Output() editMaterial = new EventEmitter<IMaterial>();
 
   public isLoading: boolean = false;
