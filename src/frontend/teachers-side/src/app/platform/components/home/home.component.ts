f0d67fc79669cefbe4b8d2fc335e6e4741f08ba3
Ekaterina Sazdova <74919977+SazdovaEkaterina@users.noreply.github.com>
2024-09-21 19:54:22 +0200

    Revert "Feature: Event Home Design (#24)"
    This reverts commit 5ca1c7245299763fc1a2bf676e665930cf512ee3.

--- src/frontend/teachers-side/src/app/platform/components/home/home.component.ts
+++ src/frontend/teachers-side/src/app/platform/components/home/home.component.ts
@@ -1,11 +1,10 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
   constructor(@Inject(Router) private router: Router) {}
